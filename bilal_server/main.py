0108b3943b16e18df700073363f704cbe7a70212
Omar Sarhan <omars@zillowgroup.com>
2021-12-27 21:52:16 -0800

    add cors

--- bilal_server/main.py
+++ bilal_server/main.py
@@ -1,14 +1,25 @@
 from fastapi import FastAPI
 from bilal_server.models import media_info
 from bilal_server.play_athan import execute_athan
+from fastapi.middleware.cors import CORSMiddleware
 
 # creating instance of FastAPI
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # just Hello World for now
 @app.get("/")
 async def root():
     return "Hello World"
+
 
 # play a sound with input as Goolge Drive file ID
 @app.post("/sound")
